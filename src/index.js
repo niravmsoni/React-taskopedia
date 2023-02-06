fe5312d93c0d6eef6b9496f1ab94e1dac8b955aa
Nirav Soni <soninirav910@gmail.com>
2023-02-06 19:53:35 +0530

    Creating first component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello World</div>);
+
+function MainBody() {
+  return(
+<div>
+    <p>In this course, we will learn react js by building TaskOPedia!</p>
+    <ul>
+      <li>Call Ben</li>
+      <li>Go to DMart</li>
+    </ul>
+  </div>
+  );
+}
+
+root.render(
+<div>
+  <MainBody/>
+  <MainBody/>
+  <MainBody/>
+</div>
+  );
