5e7bff487e7dbfb4893718e5a4acf27ddd704da4
Nirav Soni <soninirav910@gmail.com>
2023-02-07 20:43:54 +0530

    Adding a new component

--- src/index.js
+++ src/index.js
@@ -23,6 +23,26 @@ function MainBody() {
   );
 }
 
+function Students(){
+  const fullName = "Hemali Soni";
+  const programmingExp = 2;
+  return (
+    <div className="container p-4">
+      <div className = "row">
+        Students Enrolled
+      </div>
+      <div className="row border">
+      <div className="col-2">
+        <img className="w-100" src={`https://ui-avatars.com/api/?name=${fullName}`} />
+      </div>
+      <div className="col-10">
+        {fullName}<br/>
+        Coding Experience: {programmingExp} years
+      </div>
+      </div>
+    </div>
+  );
+}
 function Header(){
   return(
     <div>
@@ -59,6 +79,7 @@ root.render(
 <div>
   <Header />
   <MainBody/>
+  <Students/>
   <Footer/>
 </div>
 );
