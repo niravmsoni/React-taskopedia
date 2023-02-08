1211094b597652e4fbce486b030cd97c03bf85a0
Nirav Soni <soninirav910@gmail.com>
2023-02-08 18:09:01 +0530

    Used faker

--- src/student.jsx
+++ src/student.jsx
@@ -1,9 +1,11 @@
+import { faker } from '@faker-js/faker';
+
 export default function Student(props){
     return (
       <div className="container p-4">
         <div className="row border">
         <div className="col-2">
-          <img className="w-100" src={`https://ui-avatars.com/api/?name=${props.name}`} />
+          <img className="w-100" src={faker.image.avatar()} />
         </div>
         <div className="col-10">
           {props.name}<br/>
