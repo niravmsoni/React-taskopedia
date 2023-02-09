d34681907d1490b5f1df8d71d704b6d1c0abbdec
Nirav Soni <soninirav910@gmail.com>
2023-02-09 20:04:33 +0530

    Converted MainBody to Class component

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -1,33 +1,35 @@
 import Student from './Components/Students/Student';
 import StudentReview from './Components/Students/StudentReview';
+import React from 'react';
 
-function MainBody() {
-    const whatWeWillLearn = 'React JS';
-    const totalLecture = 3;
-    return(
-  <div style={{minHeight: "70vh"}}>
-      <p>In this course, we will learn {whatWeWillLearn} by building TaskOPedia!
-      <br/>
-      Total Lecture - {totalLecture}
-      </p>
-      <ul>
-        <li>Basic Foundation</li>
-        <li>Functional and Class Components</li>
-      </ul>
-      {/* <div>
-      Enter Task : <input maxLength={5} readOnly={true} placeholder={"test123456"}></input>
-      </div> */}
-      <div className = "col-4">Students Enrolled</div>
-        <Student experience={2} name="Hemali Soni" headShot="https://api.lorem.space/image/face?w=150&h=150">
+class MainBody extends React.Component{
+    render(){
+        const whatWeWillLearn = 'React JS';
+        const totalLecture = 3;
+        return(
+      <div style={{minHeight: "70vh"}}>
+          <p>In this course, we will learn {whatWeWillLearn} by building TaskOPedia!
+          <br/>
+          Total Lecture - {totalLecture}
+          </p>
+          <ul>
+            <li>Basic Foundation</li>
+            <li>Functional and Class Components</li>
+          </ul>
+          {/* <div>
+          Enter Task : <input maxLength={5} readOnly={true} placeholder={"test123456"}></input>
+          </div> */}
+          <div className = "col-4">Students Enrolled</div>
+            <Student experience={2} name="Hemali Soni" headShot="https://api.lorem.space/image/face?w=150&h=150">
+                <StudentReview/>
+            </Student>
+            <Student experience={5} name="Nirav Soni" headShot="https://api.lorem.space/image/face?w=151&h=150">
             <StudentReview/>
-        </Student>
-        <Student experience={5} name="Nirav Soni" headShot="https://api.lorem.space/image/face?w=151&h=150">
-        <StudentReview/>
-        </Student>
-        <Student experience={7} name="Angel Soni" headShot="https://api.lorem.space/image/face?w=152&h=150"/>
-    </div>
-    
-    );
+            </Student>
+            <Student experience={7} name="Angel Soni" headShot="https://api.lorem.space/image/face?w=152&h=150"/>
+        </div>
+        );
+    }
 }
 
 export default MainBody;
